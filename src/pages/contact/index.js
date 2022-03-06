833b7c95f33c5e8ace5c8d2a15167b80ce7eabd4
louisvno <louisvannoorden@gmail.com>
2022-03-06 12:30:06 +0100

    wworkaround for ssr issues

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,13 +2,16 @@ import * as React from "react";
 import Layout from "../../components/Layout";
 import locImg from "../../img/runakuna_location.png";
 import tel from "../../img/runakuna_tel.png";
+if (typeof window !== `undefined`) {
+  require("../../components/FormComponent")
+}
 
 export default function Index (){
 
     return (
       <Layout>
           <div className="container">
-            <div className="content">
+            <div className="content" style={{minHeight: '800px'}}>
               <div className="columns is-multiline">
                 <section className="column is-12">
                   <div style={{display:"flex", justifyContent: "center", alignItems:"center", flexDirection: "column"}}>
@@ -22,6 +25,7 @@ export default function Index (){
                     <a style={{paddingLeft: "20px"}} className="is-size-5-mobile is-size-5-tablet is-size-4-widescreen" href="tel:+34625275366">+34625275366</a>
                   </div>
                 </section>
+                {/*
                 <section className="column is-12">
                   <div style={{display:"flex", justifyContent: "center", alignItems:"center"}}>
                     <img src={locImg} alt="Ubicación" style={{maxHeight: "60px"}}/>
@@ -30,18 +34,11 @@ export default function Index (){
                       08006 Barcelona <br></br>
                     </address> 
                   </div>
-                </section>
+                </section>*/}
               </div>
 
-              <section>
-                <iframe id="JotFormIFrame-220572143589358" 
-                        title="Simple Contact Form in Spanish" 
-                        onLoad={window.parent.scrollTo(0,0)} 
-                        allowFullScreen={true} 
-                        src="https://form.jotform.com/220572143589358" 
-                        frameBorder={0} 
-                        style={{minWidth: '100%', height: '800px', border:'none'}} scrolling="no" >
-                </iframe> 
+              <section id="contact-form-container">
+
               </section>
               </div>
             </div>
